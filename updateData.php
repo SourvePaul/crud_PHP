a6c9739de5d94be22c04f5639d0999313df38ab1
SourvePaul <sourvepaul99@gmail.com>
2023-04-30 01:39:44 +0600

    qw1

--- updateData.php
+++ updateData.php
@@ -3,12 +3,12 @@
 $stu_id = $_POST['sid'];
 $stu_name = $_POST['sname'];
 $stu_address = $_POST['saddress'];
-$stu_class = $_POST['class'];
-$stu_sphone = $_POST['sphone'];
+$stu_class = $_POST['sclass'];
+$stu_phone = $_POST['sphone'];
 
 $conn3 = mysqli_connect("localhost", "root", "", "crud_php") or die("Connection failed from update data!");
 
-$sql3 = ;
+$sql3 = "UPDATE student SET sname = '{$stu_name}', saddress = '{$stu_address}', sclass = '{$stu_class}', sphone = '{$stu_phone}' WHERE sid = {$stu_id} ";
 
 $result3 = mysqli_query($conn3, $sql3) or die("Query Unsuccessful from update data!");
 
